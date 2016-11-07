fe3789eb04bdaf2f90accd599d7ad87e314d36e2
Chandan Brown <ChandanB@Chandans-MacBook-Air.local>
2016-11-06 19:27:50 -0800

    Peer to peer connectivity

--- Its Lit/ViewController.swift
+++ Its Lit/ViewController.swift
@@ -1,167 +1,38 @@
+  //
+ //  ViewController.swift
+ //  Its Lit
  //
-//  ViewController.swift
-//  Its Lit
-//
-//  Created by Chandan Brown on 8/8/16.
-//  Copyright © 2016 Gaming Recess. All rights reserved.
-//
-
-import Foundation
-import MultipeerConnectivity
-import UIKit
-import AVFoundation
-
-//Light functionality
- protocol LightServiceManagerDelegate {
-    
-    func connectedDevicesChanged(_ manager : LightServiceManager, connectedDevices: [String])
-    func lightChanged(_ manager : LightServiceManager)
-    
- }
+ //  Created by Chandan Brown on 8/8/16.
+ //  Copyright © 2016 Gaming Recess. All rights reserved.
+ //
  
- class LightServiceManager : NSObject {
-    
-    fileprivate let LightServiceType = "test-light"
-    fileprivate let myPeerId = MCPeerID(displayName: UIDevice.current.name)
-    fileprivate let serviceAdvertiser : MCNearbyServiceAdvertiser
-    fileprivate let serviceBrowser : MCNearbyServiceBrowser
-    var delegate : LightServiceManagerDelegate?
-    
-    override init() {
-        self.serviceAdvertiser = MCNearbyServiceAdvertiser(peer: myPeerId, discoveryInfo: nil, serviceType: LightServiceType)
-        
-        self.serviceBrowser = MCNearbyServiceBrowser(peer: myPeerId, serviceType: LightServiceType)
-        
-        super.init()
-        
-        self.serviceAdvertiser.delegate = self
-        self.serviceAdvertiser.startAdvertisingPeer()
-        
-        self.serviceBrowser.delegate = self
-        self.serviceBrowser.startBrowsingForPeers()
-    }
-    
-    deinit {
-        self.serviceAdvertiser.stopAdvertisingPeer()
-        self.serviceBrowser.stopBrowsingForPeers()
-    }
-    
-    lazy var session: MCSession = {
-        let session = MCSession(peer: self.myPeerId, securityIdentity: nil, encryptionPreference: MCEncryptionPreference.required)
-        session.delegate = self
-        return session
-    }()
-    
-    func sendLight() {
-        
-        if session.connectedPeers.count > 0 {
-            let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
-            if (device?.hasTorch)! {
-                
-                do {
-                    try device?.lockForConfiguration()
-                    if (device?.torchMode == AVCaptureTorchMode.on) {
-                        device?.torchMode = AVCaptureTorchMode.off
-                        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
-                        
-                    } else {
-                        do
-                        {
-                            try device?.setTorchModeOnWithLevel(1.0)
-                            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
-                        } catch {
-                            print(error)
-                        }
-                    }
-                    device?.unlockForConfiguration()
-                } catch {
-                    print(error)
-                    
-                }
-            }
-        }
-    }
- }
+ import UIKit
+ import AVFoundation
+ import MultipeerConnectivity
  
- 
- extension LightServiceManager : MCNearbyServiceAdvertiserDelegate {
-    @available(iOS 9.0, *)
-    public func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
-    }
-    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
-        NSLog("%@", "didNotStartAdvertisingPeer: \(error)")
-    }
+ class ViewController: UIViewController, MCSessionDelegate, MCBrowserViewControllerDelegate,  UINavigationControllerDelegate , UIImagePickerControllerDelegate {
     
-    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, context: Data?, invitationHandler: (@escaping (Bool, MCSession) -> Void)) {
-        
-        NSLog("%@", "didReceiveInvitationFromPeer \(peerID)")
-        invitationHandler(true, self.session)
-    }
+    @IBOutlet weak var itsLitImage: UIImageView!
     
- }
- 
- extension LightServiceManager : MCNearbyServiceBrowserDelegate {
+    //MARK: - Variables
+    var myDictionary:NSDictionary = [:]
     
-    func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
-        NSLog("%@", "didNotStartBrowsingForPeers: \(error)")
-    }
+    //Variables for Peer to Peer.
+    var browser   : MCBrowserViewController!
+    var assistant : MCAdvertiserAssistant!
+    var session   : MCSession!
+    var peerID    : MCPeerID!
     
-    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
-        NSLog("%@", "foundPeer: \(peerID)")
-        NSLog("%@", "invitePeer: \(peerID)")
-        browser.invitePeer(peerID, to: self.session, withContext: nil, timeout: 20)
-    }
     
-    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
-        NSLog("%@", "lostPeer: \(peerID)")
-    }
-    
- }
- 
- extension MCSessionState {
-    
-    func stringValue() -> String {
-        switch(self) {
-        case .notConnected: return "NotConnected"
-        case .connecting: return "Connecting"
-        case .connected: return "Connected"
-        }
-    }
-    
- }
- 
- extension LightServiceManager : MCSessionDelegate {
-    
-    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
-        NSLog("%@", "peer \(peerID) didChangeState: \(state.stringValue())")
-        self.delegate?.connectedDevicesChanged(self, connectedDevices: session.connectedPeers.map({$0.displayName}))
-    }
-    
-    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
-        NSLog("%@", "didReceiveData: \(data.count) bytes")
-    }
-    
-    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
-        NSLog("%@", "didReceiveStream")
-    }
-    
-    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL, withError error: Error?) {
-        NSLog("%@", "didFinishReceivingResourceWithName")
-    }
-    
-    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
-        NSLog("%@", "didStartReceivingResourceWithName")
-    }
-    
- }
-
-class ViewController: UIViewController {
-    
-     var audioPlayer = AVAudioPlayer()
-    @IBOutlet weak var ItsLitButton: UIImageView!
-    
+    //MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadPeerToPeer()
+        
+        var tap = UITapGestureRecognizer(target: self, action: Selector(("tappedMe")))
+        itsLitImage.addGestureRecognizer(tap)
+        itsLitImage.isUserInteractionEnabled = true
+        
         self.becomeFirstResponder()
         
         UINavigationBar.appearance().barTintColor = UIColor.rgb(254, green: 209, blue: 67)
@@ -179,35 +50,12 @@ class ViewController: UIViewController {
         
     }
     
+    //MARK: - Functions
     
-    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
-        if motion == .motionShake {
-            let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
-            if (device?.hasTorch)! {
-                do {
-                    try device?.lockForConfiguration()
-                    if (device?.torchMode == AVCaptureTorchMode.on) {
-                        device?.torchMode = AVCaptureTorchMode.off
-                        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
-
-                    } else {
-                        do {
-                            try device?.setTorchModeOnWithLevel(1.0)
-                        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
-                        } catch {
-                            print(error)
-                        }
-                    }
-                    device?.unlockForConfiguration()
-                } catch {
-                    print(error)
-                }
-            }
-            
-        }
-    }
-    
-    @IBAction func itsLit(_ sender: UIButton) {
+    func itsLitNoButton() {
+        
+       // self.present(self.browser, animated: true, completion: nil)
+    // sendInfo()
         func playSound(_ soundName: String)
         {
             let alertSound = URL(fileURLWithPath: Bundle.main.path(forResource: soundName, ofType: "aiff")!)
@@ -242,4 +90,146 @@ class ViewController: UIViewController {
         
     }
     
-}
+    func sendInfo() {
+        if self.session.connectedPeers.count > 0 {
+            let firstNameVar = "Chandan"
+            let lastNameVar = "Brown"
+            myDictionary = ["itemA" : "\(firstNameVar)", "itemB" : "\(lastNameVar)"]
+            do {
+                let data =  NSKeyedArchiver.archivedData(withRootObject: myDictionary)
+                try self.session.send(data, toPeers: self.session.connectedPeers, with: MCSessionSendDataMode.unreliable)
+            } catch let error as NSError {
+                let ac = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                present(ac, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    
+    // Called when a peer sends an NSData to us
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+        // This needs to run on the main queue
+        DispatchQueue.main.async {
+            self.itsLitNoButton()
+        }
+    }
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    func browserViewController(_ browserViewController: MCBrowserViewController, shouldPresentNearbyPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) -> Bool {
+        return true
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case MCSessionState.connected:
+            print("Connected: \(peerID.displayName)")
+        case MCSessionState.connecting:
+            print("Connecting: \(peerID.displayName)")
+        case MCSessionState.notConnected:
+            print("Not Connected: \(peerID.displayName)")
+        }
+    }
+    
+    
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+    }
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL, withError error: Error?) {
+    }
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+    }
+    
+    func loadPeerToPeer(){
+        self.peerID  = MCPeerID(displayName: UIDevice.current.name)
+        self.session = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        self.session = MCSession(peer: self.peerID)
+        self.session.delegate = self
+        self.assistant = MCAdvertiserAssistant(serviceType:"VBC-ShareCard", discoveryInfo:nil, session:self.session)
+        self.assistant.start()
+        self.browser = MCBrowserViewController(serviceType: "VBC-ShareCard", session: self.session)
+        self.browser.delegate = self
+    }
+    
+    @IBOutlet weak var ItsLitButton: UIImageView!
+    
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            sendInfo()
+            let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+            if (device?.hasTorch)! {
+                do {
+                    try device?.lockForConfiguration()
+                    if (device?.torchMode == AVCaptureTorchMode.on) {
+                        device?.torchMode = AVCaptureTorchMode.off
+                        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+                        
+                    } else {
+                        do {
+                            try device?.setTorchModeOnWithLevel(1.0)
+                            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+                        } catch {
+                            print(error)
+                        }
+                    }
+                    device?.unlockForConfiguration()
+                } catch {
+                    print(error)
+                }
+            }
+            
+        }
+    }
+    
+    
+    @IBAction func itsLit(_ sender: UIButton) {
+        
+        sendInfo()
+        func playSound(_ soundName: String)
+        {
+            let alertSound = URL(fileURLWithPath: Bundle.main.path(forResource: soundName, ofType: "aiff")!)
+            do{
+                let audioPlayer = try AVAudioPlayer(contentsOf:alertSound)
+                audioPlayer.prepareToPlay()
+                audioPlayer.play()
+            }catch {
+                print("Error getting the audio file")
+            }
+        }
+        
+        let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        if (device?.hasTorch)! {
+            do {
+                try device?.lockForConfiguration()
+                if (device?.torchMode == AVCaptureTorchMode.on) {
+                    device?.torchMode = AVCaptureTorchMode.off
+                } else {
+                    do {
+                        try device?.setTorchModeOnWithLevel(1.0)
+                        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+                    } catch {
+                        print(error)
+                    }
+                }
+                device?.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        }
+        
+    }
+    
+    @IBAction func connectScreen(_ sender: AnyObject) {
+        self.present(self.browser, animated: true, completion: nil)
+
+    }
+    
+    
+ }
